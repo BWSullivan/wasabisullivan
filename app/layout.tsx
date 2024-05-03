4dcd06ab9881b1ac17fde52bb0bb8eda709b4166
Brandon Sullivan <brandon71399@gmail.com>
2024-05-03 11:19:53 -0500

    0.0.8
    Navbar installed, brain getting bigger!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from 'next/link';
 import "./globals.css";
+import styles from './Navbar.module.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <Link href="/">
+                    wasabisullivan
+                </Link>
+            </div>
+            <ul className={styles.navLinks}>
+                <li>
+                    <Link href="/about">
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/gallery">
+                        Gallery
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/contact">
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+        {children}
+        </body>
     </html>
+  
   );
 }
