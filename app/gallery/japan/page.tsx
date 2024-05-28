09ad8fd531a3f43b6fc62a116feaa203333f9dd7
Brandon Sullivan <brandon71399@gmail.com>
2024-05-28 15:13:33 -0500

    0.1.4
    added comments n stuff

--- app/gallery/japan/page.tsx
+++ app/gallery/japan/page.tsx
@@ -37,10 +37,17 @@ const Japan: React.FC = () => {
     fetchImages();
   }, []);
 
+  // const isMobile = () => {
+  //   if (typeof(window) !== undefined) {
+  //     return window.innerWidth <= 700;
+  //   }
+  //   return false;
+  // }
+
   return (
     <div style={{ textAlign: 'center' }}>
       <h2 className="text-med text-zinc-500">
-        Full resolution images, free for personal use.
+        Full resolution images (when downloaded), free for personal use.
       </h2>
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
         {images.map((image, index) => (
@@ -51,19 +58,19 @@ const Japan: React.FC = () => {
               width="400" // size of display
               height="300"
               loading="lazy"
-              style={{ cursor: 'pointer', borderRadius: '10px' }}
-              
+              style={{ cursor: 'pointer', borderRadius: '1rem' }}
             />
           </div>
         ))}
       </div>
 
       {isOpen && (
-        <Lightbox
+        <Lightbox // User clicks on the lightbox component and it loads the full res image
+        // This is when phones crash, need to maybe lower the resolution?
           plugins={[Zoom, Fullscreen, Download]}
           open={isOpen}
           close={() => setIsOpen(false)}
-          slides={images.map(image => ({ src: `https://res.cloudinary.com/da74xkmid/image/upload/${image.src}`, alt: image.alt }))}
+          slides={images.map(image => ({src: `https://res.cloudinary.com/da74xkmid/image/upload/${image.src}`, alt: image.alt }))}
           index={photoIndex}
         />
       )}
