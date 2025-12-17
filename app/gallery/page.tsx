f3a6a324f0e94192c9d0a06b8d2ccabdd304fc8e
Brandon Sullivan <brandon71399@gmail.com>
2025-12-17 13:36:48 -0600

    0.2.3
    Imported and rendered the Navbar component in the gallery page for consistent navigation. Also updated several dependencies in package.json and package-lock.json, including Next.js, React, and ESLint, to their latest compatible versions.

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from "next/link";
+import Navbar from '../components/navbar'
 
 const navigator = [
   { name: "Japan 2023", href: "/gallery/japan" },
@@ -8,20 +9,20 @@ const navigator = [
 
 const Gallery = () => {
   return (
-    <div>
-        <nav className="my-16">
+    <><Navbar /><div>
+      <nav className="my-16">
         <ul className="flex items-center justify-center gap-5">
           {navigator.map((item) => (
-            <Link 
-            key={item.href}
-            href={item.href}
-            className="text-sm duration-500 text-white hover:text-zinc-300">
-            {item.name}
+            <Link
+              key={item.href}
+              href={item.href}
+              className="text-sm duration-500 text-white hover:text-zinc-300">
+              {item.name}
             </Link>
           ))}
         </ul>
-        </nav>
-    </div>
+      </nav>
+    </div></>
   )
 }
 
